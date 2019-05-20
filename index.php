ac7e49c514d6d438c9f982dca99a9d6e7b8455b4
yunsyazwani <yunsyazwani@gmail.com>
2019-05-20 15:51:35 +0800

    update index
    update index

--- index.php
+++ index.php
@@ -2,9 +2,54 @@
 <html>
   <?php include("head.php"); ?>
     <body>
-      <?php include("navbar.php"); ?>
+    	<div class="wrapper">
+      		<?php include("navbar.php"); ?>
 
+      		<div id="content">
+      			<?php include("top-menu.php"); ?>
+			      <div class="row">
+			      	<div class="col-md-6 padding-topbottom">
+			      		<div class="card">
+						  <div class="card-header">
+						    Featured
+						  </div>
+						  <div class="card-body">
+						    <h5 class="card-title">Special title treatment</h5>
+						    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+			      	</div>
+
+			      	<div class="col-md-6 padding-topbottom">
+			      		<div class="card">
+						  <div class="card-header">
+						    Featured
+						  </div>
+						  <div class="card-body">
+						    <h5 class="card-title">Special title treatment</h5>
+						    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+			      	</div>
+
+			      	<div class="col-md-12 padding-topbottom">
+			      		<div class="card">
+						  <div class="card-header">
+						    Featured
+						  </div>
+						  <div class="card-body">
+						    <h5 class="card-title">Special title treatment</h5>
+						    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+			      	</div>
+			      </div>
+			</div>
+      	</div>
     </body>
   <?php include("footer.php"); ?>
-  
+
 </html>
