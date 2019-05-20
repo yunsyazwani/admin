e56c178c9f52d3dd6b7d2e3f166900d43c287a71
yunsyazwani <yunsyazwani@gmail.com>
2019-05-20 15:19:36 +0800

    active menu
    done

--- dist/js/active.js
+++ dist/js/active.js
@@ -4,4 +4,11 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 
+     $(".nav-item a").click(function() {
+      // remove classes from all
+      $(".nav-item a").removeClass("active");
+      // add class to the one we clicked
+      $(this).addClass("active");
+   });
+
 });
