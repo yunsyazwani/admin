71230da29a4635abd03e857fd8517b6d97045cf9
yunsyazwani <yunsyazwani@gmail.com>
2019-05-20 16:06:53 +0800

    update card
    update card

--- index.php
+++ index.php
@@ -8,28 +8,41 @@
       		<div id="content">
       			<?php include("top-menu.php"); ?>
 			      <div class="row">
-			      	<div class="col-md-6 padding-topbottom">
-			      		<div class="card">
-						  <div class="card-header">
-						    Featured
+			      	<div class="col-md-4 padding-topbottom">
+			      		<div class="card text-white bg-success">
+						 <div class="card-body">
+						    <i class="fas fa-map-marker-alt fa-6x right card-icon"></i>
+						    <h1 class="card-title">145</h1>
+						    <p class="card-text">List of Documents</p>
 						  </div>
-						  <div class="card-body">
-						    <h5 class="card-title">Special title treatment</h5>
-						    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  <div class="card-footer">
+						    2 days ago
 						  </div>
 						</div>
 			      	</div>
 
-			      	<div class="col-md-6 padding-topbottom">
-			      		<div class="card">
-						  <div class="card-header">
-						    Featured
-						  </div>
+			      	<div class="col-md-4 padding-topbottom">
+			      		<div class="card text-white bg-danger">
 						  <div class="card-body">
-						    <h5 class="card-title">Special title treatment</h5>
-						    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  	 <i class="fas fa-file-alt fa-6x right card-icon"></i>
+						    <h1 class="card-title">23</h1>
+						    <p class="card-text">List of Location</p>
+						  </div>
+						  <div class="card-footer">
+						    2 days ago
+						  </div>
+						</div>
+			      	</div>
+
+			      	<div class="col-md-4 padding-topbottom">
+			      		<div class="card text-white bg-warning">
+						 <div class="card-body">
+						 	 <i class="fas fa-building fa-6x right card-icon"></i>
+						    <h1 class="card-title">12</h1>
+						    <p class="card-text">List of Department</p>
+						  </div>
+						  <div class="card-footer">
+						    2 days ago
 						  </div>
 						</div>
 			      	</div>
