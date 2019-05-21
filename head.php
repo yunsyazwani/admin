eeb5cd96d1fcde1e8b61cdfb8d6a51df3ef137a8
yunsyazwani <yunsyazwani@gmail.com>
2019-05-21 14:44:42 +0800

    preview pdf still problem
    preview still problem

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Admin</title>
 
+    <link href="dist/css/jquery-ui.css" rel="stylesheet" />
     <link href="dist/css/bootstrap.css" rel="stylesheet" />
     <link href="dist/css/dataTables.bootstrap.css" rel="stylesheet" />
     <link href="dist/css/fontawesome.css" rel="stylesheet" />
